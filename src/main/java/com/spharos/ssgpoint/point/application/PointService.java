19b840b3554d355d91d9b99925b9c5d5a860a684
danzieun <jieun3744@gmail.com>
2023-08-16 16:51:39 +0900

    feat: 포인트 카드 생성

--- src/main/java/com/spharos/ssgpoint/point/application/PointService.java
+++ src/main/java/com/spharos/ssgpoint/point/application/PointService.java
@@ -1,6 +1,7 @@
 package com.spharos.ssgpoint.point.application;
 
 import com.spharos.ssgpoint.point.dto.PointGetDto;
+import com.spharos.ssgpoint.pointcard.dto.PointCardGetDto;
 
 import java.util.List;
 
