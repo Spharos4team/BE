f190af1f2919199bf3e77b50279bd5ce32f320ca
Ahnyongju <for4014@gmail.com>
2023-08-24 16:42:43 +0900

    fix : 쿠폰 도메인 수정

--- src/main/java/com/spharos/ssgpoint/coupon/domain/Coupon.java
+++ src/main/java/com/spharos/ssgpoint/coupon/domain/Coupon.java
@@ -1,31 +1,63 @@
-    package com.spharos.ssgpoint.coupon.domain;
+package com.spharos.ssgpoint.coupon.domain;
 
-    import jakarta.persistence.*;
-    import lombok.AllArgsConstructor;
-    import lombok.Builder;
-    import lombok.Getter;
-    import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-    @Entity
-    @Getter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public class Coupon {
+import java.util.Date;
+import java.util.Random;
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
 
-        @Column(length = 45, nullable = true)
-        private String title;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        @Column(nullable = false)
-        private int number;
+    @Column(nullable = false, unique = true, length = 20)
+    private Integer number;
 
-        @Column(length = 255, nullable = true)
-        private String barCode;
+    @Column(length = 45, nullable = false)
+    private String title;
 
-        @Column(length = 45)
-        private String store;
+    @Column(length = 45, nullable = false)
+    private String description;
+
+    private String image;
+
+    private String content;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private Date regDate;
+
+    @Column(length = 45)
+    private String store;
+
+    private String barcode;
+
+    public boolean isValid() {
+        Date now = new Date();
+        return !now.before(startDate) && !now.after(endDate);
+    } // 닫는 중괄호 추가
+
+    private String generateCouponNumber(String storePrefix) {
+        if (storePrefix.length() != 8) {
+            throw new IllegalArgumentException("가맹점 코드는 8자리여야 합니다.");
+        }
+
+        Random random = new Random();
+        String randomDigits = String.format("%012d", random.nextLong() % 1000000000000L);
+
+        return storePrefix + randomDigits;
     }
+
+}
