39992b0a3429fbf6285b61cb2c54cab79be71b16
danzieun <jieun3744@gmail.com>
2023-08-30 09:14:58 +0900

    refactor: 클래스 이름 변경 후 LocalDate -> LocalDateTime 변경

--- src/main/java/com/spharos/ssgpoint/global/BaseEntity.java
+++ src/main/java/com/spharos/ssgpoint/global/BaseEntity.java
@@ -8,19 +8,19 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class Date {
+public class BaseEntity {
 
     @CreatedDate
     @Column(name = "created_date")
-    private LocalDate createdDate;
+    private LocalDateTime createdDate;
 
     @LastModifiedDate
     @Column(name = "updated_date")
-    private LocalDate updatedDate;
+    private LocalDateTime updatedDate;
 
 }
