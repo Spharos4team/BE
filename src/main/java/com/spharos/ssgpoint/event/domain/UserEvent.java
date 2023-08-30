a1714630056d0bb1c77e8a0faabc3f9abd4d5a04
Ahnyongju <for4014@gmail.com>
2023-08-30 14:22:50 +0900

    fix : UserEvent User클래스와 관계설정. uuid 사용

--- src/main/java/com/spharos/ssgpoint/event/domain/UserEvent.java
+++ src/main/java/com/spharos/ssgpoint/event/domain/UserEvent.java
@@ -1,5 +1,6 @@
 package com.spharos.ssgpoint.event.domain;
 
+import com.spharos.ssgpoint.user.domain.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,13 +18,18 @@ public class UserEvent {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, length = 100, nullable = false)
+    private String uuid;
+
     @Column(nullable = false, columnDefinition = "int default 0")
     private Integer status;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id") // 외래 키 컬럼명을 user_id로 설정
+    private User user; // User와의 관계 설정
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private Event event;
 
-    @ManyToOne
-    private String UUID;
-}
 
+}
