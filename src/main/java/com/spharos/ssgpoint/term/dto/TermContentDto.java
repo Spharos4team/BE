9b1421e9c951c71c02fb8810e4bb1e1f11d984cd
pjm127 <mikeywc40@gmail.com>
2023-08-31 11:09:19 +0900

    "추가안된거 추가"

---
+++ src/main/java/com/spharos/ssgpoint/term/dto/TermContentDto.java
@@ -0,0 +1,14 @@
+package com.spharos.ssgpoint.term.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TermContentDto {
+    private String termContent;
+}
---
+++ src/main/java/com/spharos/ssgpoint/user/dto/UserSavePointDto.java
@@ -0,0 +1,13 @@
+package com.spharos.ssgpoint.user.dto;
+
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserSavePointDto {
+    private int savePoint;
+}
