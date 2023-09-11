89fde705eeb7ed96b4ed0c0923490ca48441c89d
Ahnyongju <for4014@gmail.com>
2023-09-11 19:53:22 +0900

    feat: 1:1문의에 코멘트 도메인 따로구성

---
+++ src/main/java/com/spharos/ssgpoint/question/dto/CommentDTO.java
@@ -0,0 +1,21 @@
+package com.spharos.ssgpoint.question.dto;
+
+import com.spharos.ssgpoint.question.domain.Comment;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommentDTO {
+    private Long id;
+    private String uuid;
+    private String content;
+
+    public Comment toEntity() {
+        Comment comment = new Comment();
+        comment.setId(this.id);
+        comment.setUuid(this.uuid);
+        comment.setComment(this.content);
+        return comment;
+    }
+}
