24e44540781144dd6fe166be321a3a00c87c583b
pjm127 <mikeywc40@gmail.com>
2023-09-12 20:07:04 +0900

    "토큰 파일 삭제"

--- src/test/java/com/spharos/ssgpoint/SsgpointApplicationTests.java
+++ src/test/java/com/spharos/ssgpoint/SsgpointApplicationTests.java
@@ -1,13 +1,17 @@
 package com.spharos.ssgpoint;
 
+import com.spharos.ssgpoint.point.application.PointServiceImpl;
+import com.spharos.ssgpoint.point.domain.*;
+import com.spharos.ssgpoint.user.domain.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.spharos.ssgpoint.point.domain.PointStatusType.적립;
+import static com.spharos.ssgpoint.point.domain.PointType.결제;
 
 @SpringBootTest
 class SsgpointApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
