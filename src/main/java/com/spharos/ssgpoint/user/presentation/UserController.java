b6703cc4f22edd94b6ebed5bcf9d80ed818acf86
pjm127 <mikeywc40@gmail.com>
2023-09-13 13:54:11 +0900

    " @CrossOrigin 변경"

--- src/main/java/com/spharos/ssgpoint/user/presentation/UserController.java
+++ src/main/java/com/spharos/ssgpoint/user/presentation/UserController.java
@@ -23,7 +23,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1")
 @Slf4j
-@CrossOrigin(allowedHeaders = "*")
+//@CrossOrigin(allowedHeaders = "*",origins = "*")
 @RequiredArgsConstructor
 public class UserController {
 
